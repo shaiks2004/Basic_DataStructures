df34ed76781a60edd6baa3e6eb6a0966c488b20f
shaiks2004 <sau84746@gmail.com>
2026-06-22 14:21:57 +0530

    intial commit

---
+++ 1D_ Array_Operations/Running_Sum.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    int arr[]={1,2,3,4,5,6,6,7,8};
+    int size=sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=1;i<size;i++){
+        arr[i]+=arr[i-1];
+    }
+
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+
+
+}
